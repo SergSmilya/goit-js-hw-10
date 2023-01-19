60a7e416206758199304c56d5b561ec07ec47b01
SergSmilya <sergsmilichenko@gmail.com>
2023-01-19 20:33:05 +0200

    task v1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const URL = 'https://restcountries.com/v3.1/name/';
+
+const refs = {
+  input: document.getElementById('search-box'),
+  ul: document.querySelector('.country-list'),
+  div: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener(
+  'input',
+  debounce(onSercheContries, DEBOUNCE_DELAY)
+);
+
+function onSercheContries(e) {
+  const valueInput = e.target.value.trim();
+
+  fetchCountries(valueInput)
+    .then(data => console.log(data))
+    .catch(error => error);
+}
+
+function fetchCountries(name) {
+  return fetch(`${URL}${name}`).then(response => {
+    return response.json();
+  });
+}
